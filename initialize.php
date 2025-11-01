de836a0f42cbfe4371f942e0a68a00df6c197b21
marchayoh <marchayoh@gmail.com>
2025-11-01 15:42:03 +0800

    username

--- initialize.php
+++ initialize.php
@@ -4,7 +4,7 @@ if(!defined('base_url')) define('base_url','https://onlineleavesystem.infinityfr
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"sql100.infinityfree.com");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"if0_40290637 ");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"if0_40290637");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"NxaTOZQNH28PVR");
 if(!defined('DB_NAME')) define('DB_NAME',"if0_40290637_leave_db");
 ?>
